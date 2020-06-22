3aed0d34613fb05b0eb20c7f93b70bbf1b12c389
anirket <anirajkul01@gmail.com>
2020-06-23 00:24:06 +0530

    weather-website

---
+++ test.js
@@ -0,0 +1,59 @@
+window.addEventListener('load', () => {
+    let longitude;
+    let latitude;
+    let description = document.querySelector('.temprature-description');
+    let degree = document.querySelector('.temprature-degree');
+    let timezone1 = document.querySelector('.location-timezone');
+    let icon = document.querySelector('.icon');
+    let temperaturesection = document.querySelector('.degree-section');
+    let logo = document.querySelector('.degree-section span');
+    let currentlocationh5 = document.querySelector('.current-location h5');
+    let currentlocationh6 = document.querySelector('.current-location h6');
+    let error = document.querySelector('.error');
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            longitude = position.coords.longitude;
+            latitude = position.coords.latitude;
+            // const proxy = `https://cors-anywhere.herokuapp.com/`;
+            const api = `http://api.weatherapi.com/v1/current.json?key=cf3497c6d54e4930a2d173028202206&q=${latitude},${longitude}`;
+            fetch(api)
+                .then(data1 => {
+                    return data1.json();
+                })
+                .then(data => {
+                    console.log(data);
+
+                    const { temp_f, condition } = data.current;
+                    degree.textContent = temp_f;
+                    description.textContent = condition.text;
+                    timezone1.textContent = data.location.tz_id;
+                    icon.innerHTML = `<img src="${data.current.condition.icon}"<>`;
+                    //change to celcius
+                    temperaturesection.addEventListener('click', () => {
+                        if (logo.textContent === "F") {
+                            logo.textContent = "C"
+                            degree.textContent = data.current.temp_c;
+                        }
+                      else{
+                        logo.textContent = "F";
+                        degree.textContent = temp_f;
+                      }
+                    })
+                    currentlocationh5.textContent = data.location.name;
+                    currentlocationh6.textContent = data.location.region;
+
+
+                });
+        });
+
+
+    }
+    else{
+error.textContent = "please allow location for smooth usage";
+
+    }
+
+
+
+});
